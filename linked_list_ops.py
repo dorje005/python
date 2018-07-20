e01c10ff968b42280b2fc2203def1dd0ff0c9e83
Ongda Dorjee <dorje005@umn.edu>
2018-07-20 13:34:00 -0500

    Refine insertion_sort_linked_list

--- linked_list_ops.py
+++ linked_list_ops.py
@@ -19,28 +19,29 @@ def insertion_sort_linked_list(linked_list):
 	if size(linked_list) == 0:
 		return linked_list
 	curr_node = linked_list.get_head()
-	min_value_pos = 0
-	while curr_node.get_next() is not None:
-		print_list(linked_list)
-		min_value_pos = min_value_pos + 1
+	curr_pos = 0
+	while curr_node is not None:
+		must_swap = False
+		min_pos = curr_pos
 		min_value = curr_node.get_data()
 		iter = curr_node.get_next()
-		i = 0
+		i = curr_pos + 1
 		while iter is not None:
 			if iter.get_data() < min_value:
 				min_value = iter.get_data()
-				min_value_pos = i
+				min_pos = i
+				must_swap = True
+			iter = iter.get_next()
 			i = i + 1
-			if iter.get_next() is None:
-				break
-			iter = iter.get_next()
-		curr_node_value = curr_node.get_data()
-		curr_node.set_data(min_value)
-		iter = curr_node.get_next()
-		k = 0
-		while k < min_value_pos and iter.get_next() is not None:
-			iter = iter.get_next()
-			k = k + 1
-		iter.set_data(curr_node_value)
+		while must_swap:
+			curr_node_value = curr_node.get_data()
+			curr_node.set_data(min_value)
+			iter = curr_node.get_next()
+			k = curr_pos + 1
+			while k < min_pos:
+				iter = iter.get_next()
+				k = k + 1
+			iter.set_data(curr_node_value)
+			must_swap = False
 		curr_node = curr_node.get_next()
 	return linked_list
