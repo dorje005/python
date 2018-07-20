c92b2352d4b35daf50435433086b3d8d983ece69
G554025 <Dreamchasers1234!>
2018-07-20 13:26:58 -0500

    refine insertion_sort_linked_list

---
+++ linked_list_ops.py
@@ -0,0 +1,47 @@
+def size(linked_list):
+	head = linked_list.get_head()
+	if head is None:
+		return 0
+	i = 1
+	while head.get_next() is not None:
+		i = i + 1
+		head = head.get_next()
+	return i
+
+def print_list(linked_list):
+	head = linked_list.get_head()
+	while head is not None:
+		print(head.get_data(), end= " ")
+		head = head.get_next()
+	print("")
+
+def insertion_sort_linked_list(linked_list):
+	if size(linked_list) == 0:
+		return linked_list
+	curr_node = linked_list.get_head()
+	curr_pos = 0
+	while curr_node is not None:
+		must_swap = False
+		min_pos = curr_pos
+		min_value = curr_node.get_data()
+		iter = curr_node.get_next()
+		i = curr_pos + 1
+		while iter is not None:
+			if iter.get_data() < min_value:
+				min_value = iter.get_data()
+				min_pos = i
+				must_swap = True
+			iter = iter.get_next()
+			i = i + 1
+		while must_swap:
+			curr_node_value = curr_node.get_data()
+			curr_node.set_data(min_value)
+			iter = curr_node.get_next()
+			k = curr_pos + 1
+			while k < min_pos:
+				iter = iter.get_next()
+				k = k + 1
+			iter.set_data(curr_node_value)
+			must_swap = False
+		curr_node = curr_node.get_next()
+	return linked_list
