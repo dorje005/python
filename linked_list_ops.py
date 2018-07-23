8ed0ac5738ae7b3c4af59b698063b4adf2398fb3
Ongda Dorjee <dorje005@umn.edu>
2018-07-22 20:13:26 -0500

    remove redundant code
    stupid ass merge conflict

--- linked_list_ops.py
+++ linked_list_ops.py
@@ -1,9 +1,7 @@
 def size(linked_list):
 	head = linked_list.get_head()
-	if head is None:
-		return 0
-	i = 1
-	while head.get_next() is not None:
+	i = 0
+	while head is not None:
 		i = i + 1
 		head = head.get_next()
 	return i
@@ -14,38 +12,7 @@ def print_list(linked_list):
 		print(head.get_data(), end= " ")
 		head = head.get_next()
 	print("")
-
-def insertion_sort_linked_list(linked_list):
-	if size(linked_list) == 0:
-		return linked_list
-	curr_node = linked_list.get_head()
-	curr_pos = 0
-	while curr_node is not None:
-		must_swap = False
-		min_pos = curr_pos
-		min_value = curr_node.get_data()
-		iter = curr_node.get_next()
-		i = curr_pos + 1
-		while iter is not None:
-			if iter.get_data() < min_value:
-				min_value = iter.get_data()
-				min_pos = i
-				must_swap = True
-			iter = iter.get_next()
-			i = i + 1
-		while must_swap:
-			curr_node_value = curr_node.get_data()
-			curr_node.set_data(min_value)
-			iter = curr_node.get_next()
-			k = curr_pos + 1
-			while k < min_pos:
-				iter = iter.get_next()
-				k = k + 1
-			iter.set_data(curr_node_value)
-			must_swap = False
-		curr_node = curr_node.get_next()
-	return linked_list
-
+	
 def delete_duplicates(linked_list):
 	if size(linked_list) <= 1:
 		return linked_list
@@ -60,21 +27,6 @@ def delete_duplicates(linked_list):
 		curr_head = curr_head.get_next()
 	return linked_list
 	
-def size(linked_list):
-	head = linked_list.get_head()
-	i = 0
-	while head is not None:
-		i = i + 1
-		head = head.get_next()
-	return i
-
-def print_list(linked_list):
-	head = linked_list.get_head()
-	while head is not None:
-		print(head.get_data(), end= " ")
-		head = head.get_next()
-	print("")
-
 def insertion_sort_linked_list(linked_list):
 	if size(linked_list) == 0:
 		return linked_list
