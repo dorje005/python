fb097b4540fc6760edff0096e4bf76fc4e9f0df4
Ongda <dorje005@umn.edu>
2018-07-23 19:34:07 -0500

    add_node functions to linked_listclass

--- linked_list.py
+++ linked_list.py
@@ -1,13 +1,26 @@
 class linked_list:
 	def __init__(self, head=None):
 		self._head = head
-	
+
 	def get_head(self):
 		return self._head
-		
+
 	def set_head(self, head):
 		self._head = head
-	
+
+	def add_node_to_tail(self, node):
+		curr = self._head
+		while curr.get_next() is not None:
+			curr = curr.get_next()
+		curr.set_next(node)
+		return
+
+	def add_node_to_head(self, node):
+		prev_head = self._head
+		self._head = node
+		node.set_next(prev_head)
+		return
+
 	def delete_node(self, start, data):
 		curr = self.get_head()
 		if start == curr:
@@ -18,28 +31,26 @@ class linked_list:
 			curr = curr.get_next()
 		if curr is None:
 			print("Error: node not in the linked list")
-			return 
+			return
 		while curr is not None:
 			if curr.get_data() == data:
 				prev.set_next(curr.get_next())
 				return
 			curr = curr.get_next()
-		
+
 class node:
 	def __init__(self, data):
 		self._data = data
 		self._node = None
-	
+
 	def get_data(self):
 		return self._data
-	
+
 	def get_next(self):
 		return self._node
-	
+
 	def set_data(self, data):
 		self._data = data
-	
+
 	def set_next(self, node):
 		self._node = node
-	
-	
--- linked_list_ops.py
+++ linked_list_ops.py
@@ -12,7 +12,7 @@ def print_list(linked_list):
 		print(head.get_data(), end= " ")
 		head = head.get_next()
 	print("")
-	
+
 def delete_duplicates(linked_list):
 	if size(linked_list) <= 1:
 		return linked_list
@@ -26,7 +26,7 @@ def delete_duplicates(linked_list):
 			start = start.get_next()
 		curr_head = curr_head.get_next()
 	return linked_list
-	
+
 def insertion_sort_linked_list(linked_list):
 	if size(linked_list) == 0:
 		return linked_list
@@ -61,14 +61,53 @@ def insertion_sort_linked_list(linked_list):
 def find_nth_last_element(linked_list, n):
 	num_nodes = size(linked_list)
 	if num_nodes == 0:
-		return 
+		return
 	if num_nodes == n:
 		return linked_list.get_head()
 	iter = linked_list.get_head()
 	nodes_to_traverse = num_nodes - n
-	i = 0 
+	i = 0
 	while i < nodes_to_traverse and iter is not None:
 		iter = iter.get_next()
 		i = i + 1
 	return iter
-	
+
+def add_linked_list(list_a, list_b):
+	if size(list_a) == 0 and size(list_b) == 0:
+		return
+	elif size(list_a) == 0:
+		return list_b
+	elif size(list_b) == 0:
+		return list_a
+	else:
+		num_nodes_a = size(list_a)
+		num_nodes_b = size(list_b)
+		iterateA, iterateB = False, False
+		if num_nodes_a >= num_nodes_b:
+			iterateA = True
+		else:
+			iterateB = True
+		iter_a = list_a.get_head()
+		iter_b = list_b.get_head()
+		result = linked_list()
+		carry_over = 0
+		while iterateA and iter_a is not None:
+			if iter_b is None:
+				if size(result) == 0:
+					result.set_head(iter_a)
+				else:
+					result.add_node_to_tail(iter_a)
+				iterateA = False
+			else:
+				sum = iter_b.get_data() + iter_a.get_data() + carry_over
+				if sum > 9:
+					carry_over = 1
+				else:
+					carry_over = 0
+				sum_node = node(sum % 10)
+				if size(result) == 0:
+					result.set_head(sum_node)
+				else:
+					result.add_node_to_tail(sum_node)
+				iter_a = iter_a.get_next()
+		return result
